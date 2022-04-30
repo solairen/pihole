1ad4965707404db4fe4bb6edabca681c4c9745d6
Michał O <45487024+solairen@users.noreply.github.com>
2022-04-30 15:36:29 +0200

    Major improvements (#31)

--- scripts/change_variable.sh
+++ scripts/change_variable.sh
@@ -2,32 +2,32 @@
 
 function inventory() {
     # Remove old files
-    rm -f /ansible/inventory.yml /scripts/templates/temp.yml
+    rm -f /ansible/inventory.yml /scripts/templates/temp.ini
 
     # Set temp.yml
-    ( echo "cat <<EOF >/ansible/inventory.yml";
-      cat /scripts/templates/pihole-inventory-template.yml;
+    ( echo "cat <<EOF >/ansible/inventory.ini";
+      cat /scripts/templates/pihole-inventory-template.ini;
       echo;
       echo "EOF";
-    ) >/scripts/templates/temp.yml
+    ) >/scripts/templates/temp.ini
 
     # Run temp.yml
-    . /scripts/templates/temp.yml
+    . /scripts/templates/temp.ini
 }
 
 function common(){
     # Remove old files
-    rm -f /ansible/group_vars/all/common /scripts/templates/common-tmp
+    rm -f /ansible/group_vars/all/common /scripts/templates/common-tmp.yml
 
     # Set temp.yml
-    ( echo "cat <<EOF >/ansible/group_vars/all/common";
-      cat /scripts/templates/pihole-common-template;
+    ( echo "cat <<EOF >/ansible/group_vars/all/common.yml";
+      cat /scripts/templates/pihole-common-template.yml;
       echo;
       echo "EOF";
-    ) >/scripts/templates/common-tmp
+    ) >/scripts/templates/common-tmp.yml
 
     # Run temp.yml
-    . /scripts/templates/common-tmp 
+    . /scripts/templates/common-tmp.yml
 }
 
 inventory
